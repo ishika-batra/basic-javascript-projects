31ea0341a3d99ef9d336c1f2ac3e83066c22331e
ishika2203 <ishikabatra.net@gmail.com>
2023-07-27 12:59:16 +0530

    lame jokes added

--- 16-lame-jokes/app.js
+++ 16-lame-jokes/app.js
@@ -1,1 +1,32 @@
+const url = 'https://icanhazdadjoke.com/';
+
 console.log('Dad Jokes Starter');
+
+const btn = document.querySelector('.btn');
+const result = document.querySelector('.result');
+
+btn.addEventListener('click', () => {
+  fetchDadJoke();
+});
+
+const fetchDadJoke = async () => {
+  result.textContent = 'Loading...';
+
+  try {
+    const response = await fetch(url, {
+      headers: {
+        Accept: 'application/json',
+        'user-Agent': 'learning app',
+      },
+    });
+    if (!response.ok) {
+      throw new Error('There was an error!');
+    }
+    const data = await response.json();
+    result.textContent = data.joke;
+  } catch (error) {
+    result.textContent = 'There was an error!';
+  }
+};
+
+fetchDadJoke();
